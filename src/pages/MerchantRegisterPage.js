832d210706ddf77d962afd32aaf5d65b813b0d53
jrhe1 <jiarong1213@gmail.com>
2017-11-16 12:58:27 -0500

    merchant register page

--- src/pages/MerchantRegisterPage.js
+++ src/pages/MerchantRegisterPage.js
@@ -5,6 +5,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 
 import * as apiManager from '../helpers/apiManager';
+import { opay_url } from '../utilities/apiUrl';
+
 
 class MerchantRegisterPage extends Component{
 
@@ -15,10 +17,15 @@ class MerchantRegisterPage extends Component{
             inputStyle: { fontSize: '19px'  },
             textFieldStyle: { width: '58%' },
             loginBtnStyle: { marginTop: '19px' },
-            paperSize: { height: '60%', width: '50%' }
+            paperSize: { height: '60%', width: '50%' },
+            merchantName: '',
+            email: ''
         }
         this.refactor = this.refactor.bind(this);
-        this.SignUp = this.SignUp.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener('resize', this.refactor);
     }
 
     refactor = () => {
@@ -44,31 +51,17 @@ class MerchantRegisterPage extends Component{
         }
     }
 
-    SignUp = () => {
+    onFieldChange = (field, e, value) => {
 
+        let updated = Object.assign({}, this.state);
+        updated[field] = value;
+        this.setState(updated);
     }
 
-    componentDidMount() {
-        window.addEventListener('resize', this.refactor);
-        let params = {
-            "Params": {
-                "Limit": "-1",
-                "Offset": "0",
-                "Extra": {
-                    "SearchType": "DATE",
-                    "SearchField": "TODAY"
-                }
-            }
-        }
-        apiManager.opayApi('http://localhost:3000/merchant/transaction_list', params, true)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            })
+    handlerSubmit = () => {
+        console.log('state: ', this.state.merchantName);
+        console.log('state: ', this.state.email);
     }
-
 
     render() {
 
@@ -78,22 +71,29 @@ class MerchantRegisterPage extends Component{
             verticalCenter
         } = styles;
 
-
         return (
             <MuiThemeProvider>
                 <div style={mainMerchantRegisterPageStyle}>
-
                     <Paper zDepth={3} style={Object.assign({}, this.state.paperSize, paperStyle )}>
+                       
                         <div style={verticalCenter}>
-                            <TextField floatingLabelText="Username" inputStyle={this.state.inputStyle}
-                                       floatingLabelStyle={this.state.floatLabelStyle} style={this.state.textFieldStyle} /><br />
-                            <TextField floatingLabelText="Email" inputStyle={this.state.inputStyle}
-                                       floatingLabelStyle={this.state.floatLabelStyle} style={this.state.textFieldStyle} /><br /><br />
-                            <RaisedButton label="Sign Up" primary={true} style={this.state.loginBtnStyle} /> <br /> <br />
-
+                            <TextField floatingLabelText="Merchant name" 
+                                        inputStyle={this.state.inputStyle}
+                                        floatingLabelStyle={this.state.floatLabelStyle} 
+                                        style={this.state.textFieldStyle}
+                                        onChange={this.onFieldChange.bind(this, 'merchantName')} /><br />
+                            <TextField floatingLabelText="Email" 
+                                        inputStyle={this.state.inputStyle}
+                                        floatingLabelStyle={this.state.floatLabelStyle} 
+                                        style={this.state.textFieldStyle}
+                                        onChange={this.onFieldChange.bind(this, 'email')} /><br /><br />
+                            <RaisedButton label="Sign Up" 
+                                            primary={true} 
+                                            style={this.state.loginBtnStyle}
+                                            onClick={() => this.handlerSubmit()} /> <br /> <br />
                         </div>
-                    </Paper>
 
+                    </Paper>
                 </div>
             </MuiThemeProvider>
         )
@@ -123,7 +123,9 @@ const styles = {
         position: 'relative',
         top: '50%',
         transform: 'translateY(-50%)'
-    }
+    },
+
+
 }
 
 export default MerchantRegisterPage;
