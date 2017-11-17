839bfd42c77f09e4ca5917f9909d9d996a875351
Jiamin Ning <jiamin@poprx.ca>
2017-11-17 10:21:45 -0500

    redux

--- src/pages/AdminLoginPage.js
+++ src/pages/AdminLoginPage.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
+
+import * as apiManager from  '../helpers/apiManager';
+import { opay_url, admin_login } from "../utilities/apiUrl";
+import { root_page, admin_page } from '../utilities/urlPath'
+import { hideHeader, showSnackbar }  from '../actions/layout_action';
 
 class AdminLoginPage extends Component{
 
@@ -13,9 +20,47 @@ class AdminLoginPage extends Component{
             inputStyle: { fontSize: '19px'  },
             textFieldStyle: { width: '58%' },
             loginBtnStyle: { marginTop: '19px' },
-            paperSize: { height: '60%', width: '50%' }
+            paperSize: { height: '60%', width: '50%' },
+            username: '',
+            password: ''
         }
         this.refactor = this.refactor.bind(this);
+        this.login = this.login.bind(this);
+        this.usernameChange = this.usernameChange.bind(this);
+        this.passwordChange = this.passwordChange.bind(this);
+        this.props.dispatch(hideHeader(true));
+    }
+
+    usernameChange = (e, newString) => {
+        this.setState({ username: newString });
+    }
+
+    passwordChange = (e, newString) => {
+        this.setState({ password: newString });
+    }
+
+    login = () => {
+
+        let params = { Params: { LoginKeyword: this.state.username, Password: this.state.password } };
+
+        apiManager.opayApi(opay_url + admin_login,params,false).then((res) => {
+
+            console.log(res.data);
+
+            if (res.data) {
+                if (res.data.Confirmation === 'Fail') {
+                    this.props.dispatch(showSnackbar(res.data.Message, false));
+                } else if (res.data.Confirmation === 'Success') {
+                    localStorage.setItem('token', res.data.Token);
+                    browserHistory.push(`${root_page}${admin_page}`);
+                }
+            }
+
+        }).catch((err) => {
+            localStorage.removeItem('token');
+            browserHistory.push(`${root_page}`);
+        });
+
     }
 
     refactor = () => {
@@ -61,11 +106,11 @@ class AdminLoginPage extends Component{
 
                     <Paper zDepth={3} style={Object.assign({}, this.state.paperSize, paperStyle )}>
                         <div style={verticalCenter}>
-                            <TextField floatingLabelText="Username" inputStyle={this.state.inputStyle}
+                            <TextField floatingLabelText="Username" inputStyle={this.state.inputStyle} onChange={(e, newString) => this.usernameChange(e,newString)}
                                        floatingLabelStyle={this.state.floatLabelStyle} style={this.state.textFieldStyle} /><br />
-                            <TextField floatingLabelText="Password" inputStyle={this.state.inputStyle}
+                            <TextField floatingLabelText="Password" inputStyle={this.state.inputStyle} onChange={(e, newString) => this.passwordChange(e,newString)}
                                        floatingLabelStyle={this.state.floatLabelStyle} style={this.state.textFieldStyle} /><br />
-                            <RaisedButton label="Login" primary={true} style={this.state.loginBtnStyle} />
+                            <RaisedButton label="Login" primary={true} style={this.state.loginBtnStyle} onClick={() => this.login()}/>
                         </div>
                     </Paper>
 
@@ -101,4 +146,4 @@ const styles = {
     }
 }
 
-export default AdminLoginPage;
+export default connect()(AdminLoginPage);
