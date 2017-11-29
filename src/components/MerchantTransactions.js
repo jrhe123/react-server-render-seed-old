4fd3cf6aca5e62a43faf93a7bb63097bca723792
jrhe1 <jiarong1213@gmail.com>
2017-11-29 17:30:38 -0500

    trans list

--- src/components/MerchantTransactions.js
+++ src/components/MerchantTransactions.js
@@ -6,6 +6,7 @@ import Drawer from 'material-ui/Drawer';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
 import Popover, {PopoverAnimationVertical} from 'material-ui/Popover';
 import {Card} from 'material-ui/Card';
@@ -14,7 +15,9 @@ import Pagination from 'material-ui-pagination';
 import moment from 'moment';
 import SearchBar from 'material-ui-search-bar'
 import ActionSearch from 'material-ui/svg-icons/action/search';
-
+import ContentClear from 'material-ui/svg-icons/content/clear';
+import DatePicker from 'material-ui/DatePicker';
+import SelectField from 'material-ui/SelectField';
 
 
 // Redux
@@ -42,29 +45,86 @@ class MerchantTransactions extends Component{
             currentPage: 1,
             transactionList: [],
             display: 10,
+
+            transactionType: 'ALL',
+            fromDate: null,
+            endDate: null
         }
     }
 
     componentDidMount() {
-        this.fetchTransaction(null);
+        this.fetchTransaction(1, 'ALL', null);
     }
 
     handleChangePage = (page) => {
-        this.fetchTransaction(page);
+        this.setState({
+            currentPage: page
+        })
+
+        let timerange = null;
+        if(this.state.fromDate && this.state.endDate){
+            timerange = this.state.fromDate+'|'+this.state.endDate;
+        }
+        this.fetchTransaction(page, this.state.transactionType, timerange);
     }
 
-    fetchTransaction = (page) => {
-        if(!page){
-            page = 1;
+    handleTransactionTypeChange = (event, index, value) => {
+        this.setState({
+            currentPage: 1,
+            transactionType: value
+        })
+
+        let timerange = null;
+        if(this.state.fromDate && this.state.endDate){
+            timerange = this.state.fromDate+'|'+this.state.endDate;
         }
+        this.fetchTransaction(1, value, timerange);
+    }
+
+    handleChangeDate = (date, field) => {
+
+        let timerange = null;
+        if(field == 'FROM'){
+            let formatted = moment(date).format('YYYY-MM-DD HH:mm:ss');
+            this.setState({
+                fromDate: formatted
+            })
+            if(this.state.endDate){
+                timerange = formatted+'|'+this.state.endDate;
+            }
+            this.fetchTransaction(1, this.state.transactionType, timerange);
+        }else{
+            let formatted = moment(date).hour(23).minute(59).second(59).format('YYYY-MM-DD HH:mm:ss');
+            this.setState({
+                endDate: formatted
+            })
+            if(this.state.fromDate){
+                timerange = this.state.fromDate+'|'+formatted;
+            }
+            this.fetchTransaction(1, this.state.transactionType, timerange);
+        }
+    }
+
+    handleClearDate = () => {
+        this.setState({
+            fromDate: null,
+            endDate: null,
+            currentPage: 1,
+        })
+        this.fetchTransaction(1, this.state.transactionType, null);
+    }
+
+    fetchTransaction = (page, transactionField, timerange) => {
         let offset = (page - 1) * parseInt(this.state.Limit);
         let params = {
             Params: {
                 Limit: this.state.Limit,
                 Offset: offset.toString(),
                 Extra: {
-                    SearchType: "",
-                    SearchField: ""
+                    TransactionType: "TransactionType",
+                    TransactionField: transactionField,
+                    SearchType: "TIMERANGE",
+                    SearchField: timerange ? timerange : ""
                 }
             }
         };
@@ -102,13 +162,74 @@ class MerchantTransactions extends Component{
             infoContainer,
             infoWrapper,
             infoStyle,
+            searchContainer,
+            datepicker,
         } = styles;
 
         return (
             <MuiThemeProvider>
-
-                    
-
+                    <div style={searchContainer}>
+                        <RaisedButton icon={ (this.state.fromDate && this.state.endDate) ? 
+                                            <ContentClear />
+                                            :
+                                            <ActionSearch />}
+                                    style={{float: 'right', marginRight: 24}}
+                                    onClick={() => this.handleClearDate()} />
+                        <div style={Object.assign({}, datepicker)}>
+                            <DatePicker 
+                                className="ui-date-picker"
+                                hintText="End" 
+                                container="inline"
+                                autoOk={true}
+                                textFieldStyle={{
+                                    width: 120,
+                                    textIndent: 12,
+                                    marginTop: -4
+                                }}
+                                style={{
+                                    height: '36px',
+                                    background: '#fff',
+                                    boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px'
+                                }}
+                                value={ this.state.endDate ? new Date(this.state.endDate) : null }
+                                onChange={(event, date) => this.handleChangeDate(date, 'END')} />
+                        </div>
+                        <div style={Object.assign({}, datepicker)}>        
+                            <DatePicker 
+                                className="ui-date-picker"
+                                hintText="From" 
+                                container="inline"
+                                autoOk={true}
+                                textFieldStyle={{
+                                    width: 120,
+                                    textIndent: 12,
+                                    marginTop: -4
+                                }}
+                                style={{
+                                    height: '36px',
+                                    background: '#fff',
+                                    boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px'
+                                }}
+                                value={ this.state.fromDate ? new Date(this.state.fromDate) : null }
+                                onChange={(event, date) => this.handleChangeDate(date, 'FROM')} />
+                        </div>  
+                        <SelectField 
+                            className="ui-search-select"
+                            style={{
+                                float: 'right',
+                                width: 120,
+                                height: '36px',
+                                background: '#fff',
+                                boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px'
+                            }}
+                            underlineStyle={{display: 'none'}}
+                            value={this.state.transactionType} 
+                            onChange={(event, index, value) => this.handleTransactionTypeChange(event, index, value)}>
+                            <MenuItem value="ALL" label="All" primaryText="All" />
+                            <MenuItem value="ALIPAY" label="Alipay" primaryText="Alipay" />
+                            <MenuItem value="WECHAT" label="Wechat" primaryText="Wechat" />
+                        </SelectField>      
+                    </div>
                     <Card style={{width: 'calc(100% - 48px)', margin: '24px auto'}}>
 
                         {
@@ -176,7 +297,6 @@ const styles = {
         padding: 0,
         textAlign: 'center'
     },
-
     infoContainer:{
         margin: '0 auto',
         height: 200,
@@ -194,6 +314,14 @@ const styles = {
         fontSize: 19,
         fontWeight: 'bold',
         wordWrap: 'break-word'
+    },
+    searchContainer:{
+        height: 36,
+        marginTop: 24
+    },
+    datepicker:{
+        position: 'relative',
+        float: 'right',
     }
 
 }
